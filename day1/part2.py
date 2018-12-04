82be55fa68185ab06937a5fda97341917f144214
Denis Anikin <ad@xfenix.ru>
2018-12-04 23:44:56 +0300

    Correct solution for day1 part1

--- day1/part2.py
+++ day1/part2.py
@@ -6,5 +6,10 @@ with open('./data.txt', 'r') as advfile:
     data = advfile.read().strip().split("\n")
     for item in data:
         result += eval(item)
+        # print(result)
+        if result in freq_list:
+            print('Yeah: ', result)
+            break
+        freq_list.append(result)
 
-print(result)
+print(freq_list)
