03e5268207d716438e923516b21dbed63445d0b6
Denis Anikin <ad@xfenix.ru>
2018-12-05 00:04:01 +0300

    Day 1 part 2 done (bruteforce solution, sorry)

--- day1/part2.py
+++ day1/part2.py
@@ -1,15 +1,17 @@
 # https://adventofcode.com/2018/day/1
+import sys
+
+
 result = 0
 freq_list = [result, ]
 
 with open('./data.txt', 'r') as advfile:
     data = advfile.read().strip().split("\n")
-    for item in data:
-        result += eval(item)
-        # print(result)
-        if result in freq_list:
-            print('Yeah: ', result)
-            break
-        freq_list.append(result)
-
-print(freq_list)
+    while True:
+        print('.', end='', flush=True)
+        for item in data:
+            result += eval(item)
+            if result in freq_list:
+                print('Yeah: ', result)
+                sys.exit(0)
+            freq_list.append(result)
