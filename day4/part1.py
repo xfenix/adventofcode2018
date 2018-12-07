06f51a00823268c429944e2e8ecda77e2258dee5
Denis Anikin <ad@xfenix.ru>
2018-12-07 03:46:34 +0300

    Day 4 part 1 solved? Probably?

--- day4/part1.py
+++ day4/part1.py
@@ -1,5 +1,6 @@
 # https://adventofcode.com/2018/day/4
 from datetime import datetime
+from collections import Counter
 
 
 EVENT_START = 0
@@ -7,6 +8,7 @@ EVENT_SLEEP = 1
 EVENT_WAKE = 2
 shifts_by_days = {}
 guards_sleep = {}
+guards_shifts = {}
 
 with open('./data.txt', 'r') as ffile:
     data_rows = ffile.read().strip().split('\n')
@@ -33,7 +35,7 @@ with open('./data.txt', 'r') as ffile:
             date=parsed_date,
         ))
 
-for _, data in shifts_by_days.items():
+for key_date, data in shifts_by_days.items():
     if 'guard' in data:
         guard_id = data['guard']
         shift_log = sorted(data['list'], key=lambda item: item['date'])
@@ -42,12 +44,37 @@ for _, data in shifts_by_days.items():
             if log_row['event'] == EVENT_SLEEP:
                 start_sleep = log_row['date']
             elif log_row['event'] == EVENT_WAKE:
-                total_sleep += int((log_row['date'] - start_sleep).seconds/60 - 1)
+                total_sleep += int((log_row['date'] - start_sleep).seconds/60) - 1
         if guard_id not in guards_sleep:
+            guards_shifts[guard_id] = {key_date: {}}
             guards_sleep[guard_id] = 0
         guards_sleep[guard_id] += total_sleep
+        guards_shifts[guard_id][key_date] = shift_log
     else:
         pass
 
 most_sleepy_list = sorted(guards_sleep.items(), key=lambda item: item[1], reverse=True)
 most_sleepy_guard = most_sleepy_list[0][0]
+
+all_diffs = []
+for _, data in guards_shifts[most_sleepy_guard].items():
+    diffs = []
+    start = None
+    for record in data:
+        if record['event'] == EVENT_SLEEP:
+            start = record['date']
+        elif record['event'] == EVENT_WAKE and start:
+            diffs.extend([minute for minute in range(start.minute, record['date'].minute)])
+    all_diffs.append(set(diffs))
+
+minute_sleep_frequency = {}
+for minute in range(0, 60):
+    for one_diff in all_diffs:
+        if minute in one_diff:
+            if minute not in minute_sleep_frequency:
+                minute_sleep_frequency[minute] = 0
+            minute_sleep_frequency[minute] += 1
+
+# in this case - 43 minute... why? 42/44/45 also valid
+print(sorted(minute_sleep_frequency.items(), key=lambda item: item[1], reverse=True))
+print(most_sleepy_guard)
