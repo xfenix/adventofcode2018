f59f3caa538f9bb654af1759253c14b974d88ae4
Denis Anikin <ad@xfenix.ru>
2018-12-07 14:04:55 +0300

    Not ready solution for day 5 part 1

---
+++ day5/part1.py
@@ -0,0 +1,30 @@
+def calc_polymer_reaction(polymer_list):
+    total_len = len(polymer_list)
+    new_polymer = []
+    index = 0
+    while True:
+        char = polymer_list[index]
+        new_polymer.append(char)
+        if index + 1 >= total_len:
+            break
+        next_char = polymer_list[index + 1]
+        if char != next_char and char.lower() == next_char.lower():
+            new_polymer.pop()
+            index += 2
+        else:
+            index += 1
+    return new_polymer
+
+
+source_polymer = list(open('data.txt', 'r').read().strip())
+step_result = source_polymer[:]
+while True:
+    # progress
+    print('.', end='', flush=True)
+    previous_len = len(step_result)
+    step_result = calc_polymer_reaction(step_result)
+    if previous_len == len(step_result):
+        # after 1360 steps...
+        print('')
+        print(''.join(step_result))
+        break
